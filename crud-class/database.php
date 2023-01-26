a4b8719fb84d9fecf2806c0e1bba45836a358b3d
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-01-26 15:00:04 +0600

    26th jan php file added

--- crud-class/database.php
+++ crud-class/database.php
@@ -13,6 +13,7 @@ private $conn = false;
 public function __construct(){
     if (!$this->conn){
         $this->mysqli = new mysqli($this->db_host,$this->db_user,$this->db_pass,$this->db_name);
+        $this->conn = true;
         if($this->mysqli->connect_error){
             array_push($this->result, $this->mysqli->connect_error);
             return false;
@@ -22,9 +23,76 @@ public function __construct(){
     }
 }
 
-public function insert(){
+//funtion to insert into the database
+public function insert($table,$params=array()){
+    if($this->tableExists($table)){
+        //print_r($params);
+
+        $table_columns = implode(',', array_keys($params));
+        $table_value = implode("','", $params);
+
+        $sql = "INSERT INTO $table ($table_columns) VALUES ('$table_value')";
+
+        if($this->mysqli->query($sql)){
+            array_push($this->result, $this->mysqli->insert_id);
+            return true;
+        }else{
+            array_push($this->result, $this->mysqli->error);
+            return false;
+        }
+
+    }else{
+        return false;
+         }
 
 }
+//Function to update row in database
+public function update($table,$params=array(),$where = null){
+    if($this->tableExists($table)){
+        $args = array();
+        foreach ($params as $key => $value){
+            $args[] = "$key = '$value'";
+        }
+        print_r($args);
+
+        echo $sql = "UPDATE $table SET " . implode(', ', $args);
+        if($where != null){
+            $sql .= " WHERE $where";
+        }
+
+        if($this->mysqli->query($sql)){
+            array_push($this->result, $this->mysqli->affected_rows);
+
+        }else{
+            array_push($this->result, $this->mysqli->error);
+        }
+    }else{
+        return false;
+    }
+
+}
+
+private function tableExists($table){
+    $sql = "SHOW TABLES FROM $this->db_name LIKE '$table'";
+    $tableInDb = $this->mysqli->query($sql);
+    if($tableInDb){
+        if($tableInDb->num_rows ==1){
+            return true;
+        }else{
+            array_push($this->result,$table."does not exist in this database.");
+            return false;
+        }
+    }
+
+}
+
+public function getResult(){
+    $val = $this->result;
+    $this->result = array();
+    return $val;
+
+}
+
 //close connection
 
 public function __destruct(){
@@ -37,4 +105,5 @@ public function __destruct(){
         return false;
     }
 }
+
 }
--- crud-class/index.php
+++ crud-class/index.php
@@ -4,4 +4,17 @@ include 'database.php';
 
 $obj = new Database();
 
+/*
+$obj->insert('students',['student_name'=>'Sahin Mahmud','age'=>'28','city'=>'Soriothpur']);
+echo "Insert result is : ";
+print_r($obj->getResult());
+*/
+
+$obj->update('students',['student_name'=>'Emon Rahman','age'=>'28','city'=>'Potuakhali'], 'id="1"');
+
+//multiple change
+//$obj->update('students',['city'=>'Potuakhali'], 'city="Dhaka"');
+echo "Update result is : ";
+print_r($obj->getResult());
+
 ?>
